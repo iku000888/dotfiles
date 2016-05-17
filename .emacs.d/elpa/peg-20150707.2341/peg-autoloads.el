d91928ed9c7d6e306dc97aaabcdc8efc76fe8d86
iku000888 <kanumaiku@gmail.com>
2016-05-17 16:30:18 +0900

    add .emacs.d

---
+++ .emacs.d/elpa/peg-20150707.2341/peg-autoloads.el
@@ -0,0 +1,15 @@
+;;; peg-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("peg.el") (22330 33475 674880 148000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; peg-autoloads.el ends here
---
+++ .emacs.d/kbd.el
@@ -0,0 +1,7 @@
+;disable key bindings that are not necessary
+;(global-set-key (kbd "<return>") (message "!!!!!!!!!!"))
+(global-set-key (kbd "<backspace>") (message "!!!!!!!!!!"))
+(global-set-key (kbd "<up>") (message "!!!!!!!!!!"))
+(global-set-key (kbd "<down>") (message "!!!!!!!!!!"))
+(global-set-key (kbd "<right>") (message "!!!!!!!!!!"))
+(global-set-key (kbd "<left>") (message "!!!!!!!!!!"))
