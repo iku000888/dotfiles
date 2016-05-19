1aebe28cad202efb6303405576333b8493f9b2ad
iku000888 <kanumaiku@gmail.com>
2016-05-19 11:26:04 +0900

    clean up

--- .emacs.d/elpa/eval-sexp-fu-20131230.1351/eval-sexp-fu-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; eval-sexp-fu-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("eval-sexp-fu.el") (22330 33472 177654
-;;;;;;  354000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; eval-sexp-fu-autoloads.el ends here
