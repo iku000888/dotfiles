d139c0dc3b18a778aee0b4206963111cd4f39cbe
Ikuru K <kanumaiku@gmail.com>
2022-04-13 10:51:36 -0700

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,45 +11,14 @@
 ;;;
 
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (package-initialize)
 (unless package-archive-contents (package-refresh-contents))
 
 (when (not (require 'use-package nil t))
   (package-install 'use-package))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;;;;;;;;
-;;;
-;;; Utilities (copied from https://github.com/rfkm/.emacs.d)
-;;;
-
-(defun my/untabify-buffer ()
-  (interactive)
-  (untabify (point-min) (point-max)))
-
-(defun my/indent-buffer ()
-  (interactive)
-  (indent-region (point-min) (point-max)))
-
-(defun my/cleanup-buffer-safe ()
-  "Perform a bunch of safe operations on the whitespace content of a buffer.
-Does not indent buffer, because it is used for a before-save-hook, and that
-might be bad."
-  (interactive)
-  (my/untabify-buffer)
-  (delete-trailing-whitespace)
-  (set-buffer-file-coding-system 'utf-8))
-
-(defun my/cleanup-buffer ()
-  "Perform a bunch of operations on the whitespace content of a buffer.
-Including my/indent-buffer, which should not be called automatically on save."
-  (interactive)
-  (my/cleanup-buffer-safe)
-  (my/indent-buffer))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;;;;;;;;
 ;;;
@@ -62,21 +31,26 @@ Including my/indent-buffer, which should not be called automatically on save."
 ;; (setq use-package-always-pin "melpa-stable")
 (setq use-package-verbose t)
 
+(use-package seq)
+
 ;;; load your preferred theme
 (use-package material-theme
   :config
   (load-theme 'material t))
 
 ;;; set up exec-path
-(use-package exec-path-from-shell
-  :config
-  (when (memq window-system '(mac ns x))
-    (exec-path-from-shell-initialize)))
+;; (use-package exec-path-from-shell
+;;   :config
+;;   (when (memq window-system '(mac ns x))
+;;     (exec-path-from-shell-initialize)))
 
 (use-package mykie
   :config
   (setq mykie:use-major-mode-key-override t)
   (mykie:initialize))
+
+(use-package docker-tramp)
+(require 'docker-tramp-compat)
 
 ;;; happy (((()))) !!!
 (use-package paren
@@ -85,16 +59,16 @@ Including my/indent-buffer, which should not be called automatically on save."
   (show-paren-mode 1))
 
 ;;; yasnippet
-(use-package yasnippet
-  :init
-  (yas-global-mode 1)
-  (bind-keys :map yas-minor-mode-map
-             ("<tab>" . nil)
-             ("TAB" . nil)
-             ("C-i" . nil)
-             ("C-o" . yas/expand)))
+;; (use-package yasnippet
+;;   :init
+;;   (yas-global-mode 1)
+;;   (bind-keys :map yas-minor-mode-map
+;;              ("<tab>" . nil)
+;;              ("TAB" . nil)
+;;              ("C-i" . nil)
+;;              ("C-o" . yas/expand)))
 
-;;; comapany-mode!
+;;comapany-mode!
 (use-package company
   :defer t
   :config
@@ -114,13 +88,13 @@ Including my/indent-buffer, which should not be called automatically on save."
              ("C-p" . company-select-previous)))
 
 ;;; projectile
-(use-package projectile
-  :init
-  (projectile-global-mode))
+;; (use-package projectile
+;;   :init
+;;   (projectile-global-mode))
 
-(use-package subword
-  :init
-  (global-subword-mode 1))
+;; (use-package subword
+;;   :init
+;;   (global-subword-mode 1))
 
 ;;; answering just 'y' or 'n' will do
 (defalias 'yes-or-no-p 'y-or-n-p)
@@ -148,7 +122,6 @@ Including my/indent-buffer, which should not be called automatically on save."
 (setq-default indicate-buffer-boundaries 'right)
 (setq uniquify-buffer-name-style 'post-forward)
 
-;; whitespace
 (use-package whitespace
   :config
   (setq whitespace-style '(face
@@ -165,13 +138,8 @@ Including my/indent-buffer, which should not be called automatically on save."
   (global-whitespace-mode 1)
   (setq-default tab-width 4 indent-tabs-mode nil))
 
-;;; cleanup whitespace before file save
-;;; (add-hook 'before-save-hook 'whitespace-cleanup)
-
-(use-package hl-line
-  :config
-  (global-hl-line-mode 1)
-  (set-face-background 'hl-line "#525252"))
+;;cleanup whitespace before file save
+(add-hook 'before-save-hook 'whitespace-cleanup)
 
 ;;; modeline
 (setq display-time-string-forms
@@ -201,26 +169,26 @@ Including my/indent-buffer, which should not be called automatically on save."
    global-mode-string
    ))
 
-(setq-default
- header-line-format
- '(""
-   (:propertize (:eval (shorten-directory default-directory 30))
-                face mode-line-folder-face)
-   (:propertize "%b"
-                face mode-line-filename-face)))
+;; (setq-default
+;;  header-line-format
+;;  '(""
+;;    (:propertize (:eval (shorten-directory default-directory 30))
+;;                 face mode-line-folder-face)
+;;    (:propertize "%b"
+;;                 face mode-line-filename-face)))
 
-(defun shorten-directory (dir max-length)
-  "Show up to `max-length' characters of a directory name `dir'."
-  (let ((path (reverse (split-string (abbreviate-file-name dir) "/")))
-        (output ""))
-    (when (and path (equal "" (car path)))
-      (setq path (cdr path)))
-    (while (and path (< (length output) (- max-length 4)))
-      (setq output (concat (car path) "/" output))
-      (setq path (cdr path)))
-    (when path
-      (setq output (concat ".../" output)))
-    output))
+;; (defun shorten-directory (dir max-length)
+;;   "Show up to `max-length' characters of a directory name `dir'."
+;;   (let ((path (reverse (split-string (abbreviate-file-name dir) "/")))
+;;         (output ""))
+;;     (when (and path (equal "" (car path)))
+;;       (setq path (cdr path)))
+;;     (while (and path (< (length output) (- max-length 4)))
+;;       (setq output (concat (car path) "/" output))
+;;       (setq path (cdr path)))
+;;     (when path
+;;       (setq output (concat ".../" output)))
+;;     output))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;;;;;;;;
 ;;;
@@ -234,15 +202,15 @@ Including my/indent-buffer, which should not be called automatically on save."
         helm-ff-transformer-show-only-basename nil)
   (bind-keys :map global-map
              ("M-x" . helm-M-x)))
-                                        ; (use-package helm-projectile
-;;   :config
-;;   (mykie:set-keys nil
-;;     "C-x C-f"
-;;     :default (call-interactively 'find-file)
-;;     :C-u helm-projectile-find-file
-;;     "C-x b"
-;;     :default (call-interactively 'switch-to-buffer)
-;;     :C-u helm-projectile-switch-to-buffer))
+                                        (use-package helm-projectile
+  :config
+  (mykie:set-keys nil
+    "C-x C-f"
+    :default (call-interactively 'find-file)
+    :C-u helm-projectile-find-file
+    "C-x b"
+    :default (call-interactively 'switch-to-buffer)
+    :C-u helm-projectile-switch-to-buffer))
 
 (use-package helm-ls-git
   :config
@@ -256,10 +224,13 @@ Including my/indent-buffer, which should not be called automatically on save."
 
 
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;;;;;;;;
 ;;;
 ;;; lisp
 ;;;
+
+(use-package aggressive-indent)
 
 (use-package paredit
   :defer t
@@ -282,8 +253,9 @@ Including my/indent-buffer, which should not be called automatically on save."
   :defer t)
 
 (defun my/lisp-mode-defaults ()
+  (aggressive-indent-mode 1)
   (paredit-mode 1)
-  (rainbow-delimiters-mode 1)
+  ;;(rainbow-delimiters-mode 1)
   (eldoc-mode 1))
 
 (defun my/lisp-mode-hook ()
@@ -298,10 +270,9 @@ Including my/indent-buffer, which should not be called automatically on save."
 
 (use-package clojure-mode
   :init
-  (add-hook 'clojure-mode-hook #'yas-minor-mode)
-  (add-hook 'clojure-mode-hook #'subword-mode)
-  (add-hook 'clojure-mode-hook #'my/lisp-mode-hook)
-  (add-hook 'before-save-hook #'my/cleanup-buffer))
+  ;;(add-hook 'clojure-mode-hook #'yas-minor-mode)
+  ;;(add-hook 'clojure-mode-hook #'subword-mode)
+  (add-hook 'clojure-mode-hook #'my/lisp-mode-hook))
 
 (use-package cider
   :init
@@ -314,53 +285,77 @@ Including my/indent-buffer, which should not be called automatically on save."
   (setq nrepl-log-messages t
         cider-repl-display-in-current-window t
         cider-repl-use-clojure-font-lock t
-        cider-prompt-save-file-on-load 'always-save
+        ;;cider-prompt-save-file-on-load 'always-save
         cider-font-lock-dynamically '(macro core function var)
         cider-overlays-use-font-lock t)
   (cider-repl-toggle-pretty-printing))
-
-(use-package midje-mode)
 
 (use-package clj-refactor
   :init   (setq cljr-favor-prefix-notation nil)
   :diminish clj-refactor-mode
   :config (cljr-add-keybindings-with-prefix "C-c j"))
 
-(message "init.el loaded!!")
+;;(message "init.el loaded!!")
 
-(add-hook 'after-init-hook
-          (lambda ()
-            (message "init time: %.3f sec"
-                     (float-time (time-subtract after-init-time before-init-time)))))
+
+;; (add-hook 'after-init-hook
+;;           (lambda ()
+;;             (message "init time: %.3f sec"
+;;                      (float-time (time-subtract after-init-time before-init-time)))))
 
 (use-package magit
   :bind ("C-x g" . magit-status))
 
-(put-clojure-indent 'fnk 'defun)
-(put-clojure-indent 'defnk 'defun)
-(put-clojure-indent 'for-map 1)
-(put-clojure-indent 'instance 2)
-(put-clojure-indent 'inline 1)
-(put-clojure-indent 'letk 1)
-(put-clojure-indent 'when-letk 1)
-(put-clojure-indent 'go-loop 1)
-(put-clojure-indent 'this-as 'defun)
-(put-clojure-indent 'when-some '1)
-(put-clojure-indent 'if-some '1)
-(put-clojure-indent 'try+ 0)
-(put 'specify 'clojure-backtracking-indent '((2)))
-(put 'specify! 'clojure-backtracking-indent '((2)))
-(put 'defcomponent 'clojure-backtracking-indent '((2)))
-(put 'defcomponentk 'clojure-backtracking-indent '((2)))
-(put 'defmixin 'clojure-backtracking-indent '((2)))
-(put 'clojure.core/defrecord 'clojure-backtracking-indent '(4 4 (2)))
-(put 's/defrecord 'clojure-backtracking-indent '(4 4 (2)))
-(put 's/defrecord+ 'clojure-backtracking-indent '(4 4 (2)))
-(put 'potemkin/deftype+ 'clojure-backtracking-indent '(4 4 (2)))
-(put 'potemkin/defrecord+ 'clojure-backtracking-indent '(4 4 (2)))
+(use-package magit
+  :bind ("C-x t" . magit-section-toggle))
+
+(use-package ddskk
+  :bind ("C-x j" . skk-mode))
+(setq skk-egg-like-newline t)
+
 
 ;;; global keymap
-(use-package bind-key
-  :config
-  (bind-keys :map global-map
-             ("C-h" . delete-backward-char)))
+;; (use-package bind-key
+;;   :config
+;;   (bind-keys :map global-map
+;;              ("C-h" . delete-backward-char)))
+(put 'set-goal-column 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(dart-mode yaml-mode ddskk cider docker-tramp magit use-package rainbow-delimiters mykie material-theme helm-projectile helm-ls-git helm-ag company clj-refactor aggressive-indent)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+(use-package lsp-mode
+  :ensure t)
+(use-package lsp-dart
+  :ensure t
+  :hook (dart-mode . lsp))
+(use-package lsp-ui
+  :ensure t)
+(use-package hover
+  :ensure t)
+
+(use-package dart-mode)
+(use-package lsp-treemacs)
+(use-package flycheck)
+(use-package hover)
+
+(add-hook 'dart-mode-hook 'lsp)
+(setq
+ lsp-dart-flutter-sdk-dir "/snap"
+ lsp-dart-sdk-dir "/snap"
+ gc-cons-threshold (* 100 1024 1024)
+ read-process-output-max (* 1024 1024)
+ company-minimum-prefix-length 1
+ lsp-lens-enable t
+ lsp-signature-auto-activate nil)
